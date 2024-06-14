04c18b407407321082fe03c4fc2ae4789e1fbdf4
kleber-matos <94052079+kleber-matos@users.noreply.github.com>
2024-06-14 17:58:35 -0300

    style: add styled 'produtos' section to main

--- src/pages/Home/Styled.jsx
+++ src/pages/Home/Styled.jsx
@@ -1,22 +1,33 @@
 import styled, { css } from "styled-components";
 
 const center = css`
-display: flex;
-justify-content: center;
-align-items: center;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const ContainerBusca = styled.section`
-border: solid 1px red;
-${center}
-justify-content: space-around;
-background-color: #454C59;
-height: 17vh;
-`
-export const BoxCep = styled.div`
-
-`
+  border: solid 1px red;
+  ${center}
+  justify-content: space-around;
+  background-color: #454c59;
+  height: 17vh;
+`;
+export const BoxCep = styled.div``;
 
 export const BoxInput = styled.div`
-border: solid 1px;
-`
+  border: solid 1px;
+`;
+
+export const ContainerProdutos = styled.section`
+  border: solid;
+  display: flex;
+  justify-content: center;
+`;
+export const BoxProdutos = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  width: 90vw;
+  max-width: 1100px;
+`;
