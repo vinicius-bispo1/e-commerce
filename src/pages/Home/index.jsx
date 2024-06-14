e2fa3335731ba4e09c0209aa3504b811d4c85ecb
vinicius-bispo1 <vini.bispo125@gmail.com>
2024-06-14 14:39:41 -0300

    :sparkles: feat:  inicio home e footer

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Header from '../../Components/Header'
 
 export default function Home() {
   return (
-    <div>home</div>
+    <main>
+        <Header/>
+    </main>
   )
 }
