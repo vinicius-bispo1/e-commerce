46cfc7ac961f50a149547b5d825200af688c818b
kleber-matos <94052079+kleber-matos@users.noreply.github.com>
2024-06-14 16:57:41 -0300

    feat: add section to main

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import Header from "../../Components/Header";
+import { FaSearch } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Header />
-    </main>
+
+      <main>
+        <section>
+          <div>
+            <FaLocationDot />
+            <h2>Informe seu CEP</h2>
+          </div>
+
+          <div>
+            <input type="text" />
+            <FaSearch />
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
