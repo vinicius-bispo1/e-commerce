aaffe0f3e6f07d7bc3c8123a9cb1058396fa7b93
kleber-matos <94052079+kleber-matos@users.noreply.github.com>
2024-06-14 22:38:44 -0300

    feat: add modal cep

--- src/pages/Home/Styled.jsx
+++ src/pages/Home/Styled.jsx
@@ -7,28 +7,28 @@ const center = css`
 `;
 
 export const ContainerBusca = styled.section`
-${center}
-justify-content: space-around;
-background-color: #454C59;
-height: 17vh;
-margin-bottom: 50px;
-`
+  ${center}
+  justify-content: space-around;
+  background-color: #454c59;
+  height: 17vh;
+  margin-bottom: 50px;
+`;
 export const BoxCep = styled.div`
-${center}
-justify-content: space-between;
-color: white;
-font-size: 20px;
-gap: 15px;
-`
+  ${center}
+  justify-content: space-between;
+  color: white;
+  font-size: 20px;
+  gap: 15px;
+`;
 export const BoxInput = styled.div`
-/* border: solid 1px; */
-font-size: 20px;
-background-color: white;
-border-radius: 5px;
-padding-right: 10px;
-${center}
-cursor: pointer;
-input{
+  /* border: solid 1px; */
+  font-size: 20px;
+  background-color: white;
+  border-radius: 5px;
+  padding-right: 10px;
+  ${center}
+  cursor: pointer;
+  input {
     border: solid 1px;
     padding: 10px;
     width: 23vw;
@@ -37,8 +37,8 @@ input{
     border-radius: 5px;
     border: none;
     outline: none;
-}
-`
+  }
+`;
 
 export const ContainerProdutos = styled.section`
   display: flex;
@@ -52,3 +52,65 @@ export const BoxProdutos = styled.section`
   max-width: 1100px;
 `;
 
+export const Modal = styled.div`
+  top: 35%;
+  width: 100vw;
+  display: flex;
+  position: absolute;
+  justify-content: center;
+`;
+
+export const ButtonModal = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 50vh;
+
+  h2 {
+    margin-bottom: 5px;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    background-color: transparent;
+    font-size: 1.5em;
+  }
+`;
+
+export const BoxModal = styled.div`
+  background-color: white;
+  border-radius: 5px;
+  padding: 1rem;
+  box-shadow: rgba(0, 0, 0, 0.56) 0px 22px 70px 4px;
+`;
+
+export const ModalCampos = styled.div`
+  p {
+    height: 4vh;
+    display: flex;
+    align-items: center;
+  }
+
+  input {
+    width: 90%;
+    height: 5vh;
+    font-size: 18px;
+    border: none;
+    border-bottom: 1px solid gray;
+    outline: none;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    background-color: transparent;
+    font-size: 1.5em;
+  }
+`;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,20 +4,34 @@ import { FaSearch } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 import { Produtos } from "../../Data";
 import Card from "../../Components/Card";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
+  const [modalCep, setModalCep] = useState(false);
+  const fundoRef = useRef(null);
+
+  useEffect(() => {
+    if (modalCep) {
+      fundoRef.current.style.filter = "blur(5px)";
+    } else {
+      fundoRef.current.style.filter = "blur(0px)";
+    }
+  }, [modalCep]);
+
   return (
     <>
+      {modalCep}
       <Header />
-      <main>
+
+      <main ref={fundoRef}>
         <S.ContainerBusca>
-          <S.BoxCep>
+          <S.BoxCep onClick={() => setModalCep(!modalCep)}>
             <FaLocationDot />
             <h2>Informe seu CEP</h2>
           </S.BoxCep>
 
           <S.BoxInput>
-            <input type="text" placeholder='busque aqui seu produto' />
+            <input type="text" placeholder="busque aqui seu produto" />
             <FaSearch />
           </S.BoxInput>
         </S.ContainerBusca>
@@ -26,7 +40,8 @@ export default function Home() {
         <S.ContainerProdutos>
           <S.BoxProdutos>
             {Produtos.map((item) => (
-              <Card style={{color: "red"}}
+              <Card
+                style={{ color: "red" }}
                 descricao={item.descricao}
                 preco={item.preco}
                 imagem={item.imagem}
@@ -36,6 +51,32 @@ export default function Home() {
           </S.BoxProdutos>
         </S.ContainerProdutos>
       </main>
+
+      {modalCep && (
+        <S.Modal>
+          <S.BoxModal>
+            <S.ButtonModal>
+              <h2>Digite seu CEP:</h2>
+
+              <button onClick={() => setModalCep(!modalCep)}>X</button>
+            </S.ButtonModal>
+            <S.ModalCampos>
+              <div>
+                <input type="text" placeholder="Informe seu CEP aqui..." />
+                <button>
+                  <FaSearch />
+                </button>
+              </div>
+
+              <p>CEP:</p>
+              <p>Rua:</p>
+              <p>Estado:</p>
+              <p>Bairro:</p>
+              <p>Bairro:</p>
+            </S.ModalCampos>
+          </S.BoxModal>
+        </S.Modal>
+      )}
     </>
   );
 }
