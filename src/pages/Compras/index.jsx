41288971ce926491a983095cf1a6d142e6ff57be
vinicius-bispo1 <vini.bispo125@gmail.com>
2024-06-17 21:11:50 -0300

    :sparkles: feat: add icone e stylo de compras

--- src/pages/Compras/index.jsx
+++ src/pages/Compras/index.jsx
@@ -1,4 +1,7 @@
 import * as S from "./Styled";
+import cesta from "../../assets/cesta.png"
+import truck from "../../assets/truck.png"
+import cartao from "../../assets/cartao.png"
 
 export default function Compras() {
   return (
@@ -6,6 +9,13 @@ export default function Compras() {
       <S.BoxTitle>
         <h2>Compras</h2>
       </S.BoxTitle>
+      <section>
+        <S.BoxIcone>
+         <img src={cesta} alt="" />
+         <img src={truck} alt="" />
+         <img src={cartao} alt="" />
+        </S.BoxIcone>
+      </section>
     </main>
   );
 }
