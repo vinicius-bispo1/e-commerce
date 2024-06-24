9775e6c7c817fd609532d07ba0117206dfcf19d3
kleber-matos <94052079+kleber-matos@users.noreply.github.com>
2024-06-24 11:51:51 -0300

    feat: add function addproduto

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -5,11 +5,13 @@ import { FaStar } from "react-icons/fa";
 import Estrela from "../Estrela";
 import { Produtos } from "../../Data";
 
-export default function Card({ descricao, preco, imagem, status }) {
+export default function Card({ descricao, preco, imagem, status, id }) {
   return (
     <S.Div>
       <S.Img src={imagem} alt="Celular" />
-      <S.P>{descricao}</S.P>
+      <S.P>
+        {descricao} - {id}
+      </S.P>
       <S.H2>R$ {preco}</S.H2>
       <Estrela />
       <S.Span cor={status}>{status}</S.Span>
